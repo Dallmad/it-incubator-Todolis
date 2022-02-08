e4c32c1f3d745b5475b0505a67555d6126d3fc34
Dzmitry <maddal.privod@gmail.com>
2022-02-08 22:19:57 +0300

    created items forms

---
+++ src/components/AddItemForm.tsx
@@ -0,0 +1,45 @@
+import React, {ChangeEvent, useState, KeyboardEvent} from "react";
+import {Button} from "../Button";
+
+type AddItemFormPropsType = {
+    addItem: (title: string)=> void
+
+}
+
+export const AddItemForm:React.FC<AddItemFormPropsType> = (props) => {
+    const [title, setTitle] = useState<string>('')
+    const [error, setError] = useState<boolean>(false)
+
+    const onKeyPressAddItem = (e: KeyboardEvent<HTMLInputElement>) =>
+        e.key === "Enter" && onClickAddItem()
+    const onChangeSetTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+        setError(false)
+    }
+    const onClickAddItem = () => {
+        const trimmedTitle = title.trim()
+        if (trimmedTitle) {
+            props.addItem(trimmedTitle)
+        } else {
+            setError(true)
+        }
+        setTitle("")
+    }
+    const errorMessage = error
+        ? <div style={{color: "white", backgroundColor: "red"}}>Title is required!</div>
+        : null
+
+    return (
+        <div>
+            <input
+                value={title}
+                onChange={onChangeSetTitle} //input.value
+                onKeyPress={onKeyPressAddItem}
+                className={error ? "error" : ""}
+            />
+            <Button onClickHandler={onClickAddItem} title={'+'} active={false}/>
+            {errorMessage}
+        </div>
+    )
+}
+
