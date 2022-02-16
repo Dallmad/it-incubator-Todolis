73d7e7a08a68fc2be9ece91a2f1e425b56ac5b82
Dzmitry <maddal.privod@gmail.com>
2022-02-16 14:07:51 +0300

    plugin materialUI

---
+++ src/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type ButtonPropsType = {
+    active: boolean
+    title: string
+    onClickHandler: () => void
+}
+
+export const Button: React.FC<ButtonPropsType> = (
+    {
+        title,
+        onClickHandler,
+        active,
+    }
+) => {
+    return (
+        <button
+            className={active ? "active" : ""}
+            onClick={onClickHandler}>
+            {title}
+        </button>
+    )
+}
