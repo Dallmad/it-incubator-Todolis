db1e603f582d9e83ab64c8c00f303b97f05eb5bf
Dzmitry <maddal.privod@gmail.com>
2022-02-23 06:49:38 +0300

    learn reducers

---
+++ src/store/tasks.ts
@@ -0,0 +1,23 @@
+export const sum = (state: number, num: number): number => state + num
+export const substruction = (state: number, num: number): number => state - num
+export const div = (state: number, num: number): number => state / num
+export const mult = (state: number, num: number): number => state * num
+
+type ActionType = {
+    type: 'SUM' | 'SUB' | 'DIV' | 'MULT'
+    num: number
+}
+export const calculatorReducer = (state: number, action: ActionType) => {
+    switch (action.type) {
+        case 'SUM':
+            return state+action.num
+        case 'SUB':
+            return state-action.num
+        case 'DIV':
+            return state/action.num
+        case 'MULT':
+            return state*action.num
+        default:
+            return state
+    }
+}
